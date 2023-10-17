037437ceb83350a96c524c5fe7b6692159638b8a
EE2SE <jakub.m.szypicyn@gmail.com>
2023-10-17 21:08:51 +0100

    fetching share price for every eps date

--- stock_class.py
+++ stock_class.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import requests
-
+from datetime import datetime, timedelta
+import pandas as pd
 
 class MyStock:
 
@@ -9,7 +10,8 @@ class MyStock:
         initilise the object
         :param ticker: string containing the ticker name
         """
-        self.__EPS = 0
+        self._eps = pd.DataFrame()
+        self._pe = 0
 
         # try fetching the ticker
         try:
@@ -24,18 +26,37 @@ class MyStock:
         # once we fetch it we get the info we want into our object
         else:
             self.__find_eps()
+            self.__calculate_pe()
 
     def get_eps(self):
-        return self.__EPS
+        return self._eps
 
     def __find_eps(self):
         # fetch the diluted EPS
         # EPS is the first step to calculating the P/E Ratio as a valuation metrics
         # While the basic EPS is a good measure of current profitability, the diluted EPS is more scientific
         # as it also includes potential dilutions and shareholders are not in for a very nasty surprise.
-        self.__EPS = self.ticker.quarterly_income_stmt.T['Diluted EPS']
+        self._eps = self.ticker.quarterly_income_stmt.T['Diluted EPS']
+
+    def get_pe(self):
+        return self._pe
+
+    def __calculate_pe(self):
+        # for every date in quarterly eps, get the share price
+        for date in self._eps.index:
+            t_start = date - timedelta(days=0, hours=0)
+            t_end = date + timedelta(days=1, hours=0)
+            data = self.ticker.history(interval="1d", period="5y", start=t_start, end=t_end)
+            increment = 2
+            while data.empty:
+                t_start = date + timedelta(days=increment-1, hours=0)
+                t_end = date + timedelta(days=increment, hours=0)
+                data = self.ticker.history(interval="1d", period="5y", start=t_start, end=t_end)
+                increment += 1
+
+            print(data)
 
 
 if __name__ == "__main__":
     a = MyStock("AAPL")
-    print(a.get_eps())
+
