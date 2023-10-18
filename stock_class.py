9eef40f93574d0f9cc4335807d9e10a4fc595b2f
EE2SE <jakub.m.szypicyn@gmail.com>
2023-10-18 13:47:40 +0100

    stores pe ratio

--- stock_class.py
+++ stock_class.py
@@ -15,7 +15,8 @@ class MyStock:
 
         # try fetching the ticker
         try:
-            self.ticker = yf.Ticker(ticker)
+            self.ticker_name = ticker
+            self.ticker = yf.Ticker(self.ticker_name)
             self.valid = True
 
         # if it doesn't exist HTTPError will be thrown
@@ -36,25 +37,35 @@ class MyStock:
         # EPS is the first step to calculating the P/E Ratio as a valuation metrics
         # While the basic EPS is a good measure of current profitability, the diluted EPS is more scientific
         # as it also includes potential dilutions and shareholders are not in for a very nasty surprise.
-        self._eps = self.ticker.quarterly_income_stmt.T['Diluted EPS']
+        self._eps = 4*self.ticker.quarterly_income_stmt.T['Diluted EPS']
 
     def get_pe(self):
         return self._pe
 
     def __calculate_pe(self):
+        self._pe = self._eps.copy()
+        self._pe.rename("PE", inplace=True)
         # for every date in quarterly eps, get the share price
         for date in self._eps.index:
             t_start = date - timedelta(days=0, hours=0)
             t_end = date + timedelta(days=1, hours=0)
             data = self.ticker.history(interval="1d", period="5y", start=t_start, end=t_end)
             increment = 2
-            while data.empty:
+            while data.empty and increment < 10:
                 t_start = date + timedelta(days=increment-1, hours=0)
                 t_end = date + timedelta(days=increment, hours=0)
                 data = self.ticker.history(interval="1d", period="5y", start=t_start, end=t_end)
                 increment += 1
+            if data.empty:
+                print("No valid share data for ticker: " + self.ticker_name + " is available.")
+            else:
+                self._pe.loc[date] = data['Close'].iloc[0]/self._eps.loc[date]
 
-            print(data)
+
+
+
+
+
 
 
 if __name__ == "__main__":
